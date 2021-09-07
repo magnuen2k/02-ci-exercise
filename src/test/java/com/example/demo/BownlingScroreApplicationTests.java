d6a168491de2aac6ae8e5f4ef9e5bdee6953aca3
Øydis Kind Refsum <oeydis@gmail.com>
2021-09-07 11:45:53 +0200

    basic roll and passing test :white_check_mark:

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BownlingScroreApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testRoll() {
+		Game g = new Game();
+		int score = g.roll(10);
+		assertTrue(score <= 10);
+		assertTrue(score >= 0);
 	}
 
 }
